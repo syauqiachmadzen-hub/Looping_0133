6dfd6315fc9d54995396d1e32bfe8741d664b6df
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-03-31 19:56:18 +0700

    commit 1

---
+++ Looping.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#includer <ctime>
+using namespace std;
+
+int main(){
+    int x;
+
+    //perulangan dengan for
+    //mencetak tulisan sebanyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl
+    for(int i=0;i<=4;i++){
+        
+    }
+}
