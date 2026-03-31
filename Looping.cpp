f4a8be9219be969a5290614a24e8db323bb2a1b7
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-03-31 21:36:29 +0700

    commit 13

--- Looping.cpp
+++ Looping.cpp
@@ -110,7 +110,17 @@ int main(){
 #includ <iostream>
 using namespace std;
 
-//Deklarasi Variable global
+// Deklarasi Variable global
 int kode jurusan;
 string namaJurusan;
 string statuskuota;
+// prosedure input 
+void input() {
+    cout << "===PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" <<endl;
+    cout << "3. Sistem Informasi" <<endl;
+    cout << ":-------------------------------" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    
+}
